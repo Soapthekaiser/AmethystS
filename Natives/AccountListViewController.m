72142749900a9716999a2a537d42545a3a712cd6
Soapthekaiser <jhonperseusmactavish@gmail.com>
2026-05-29 19:21:39 -0400

    Remove unused methods and clean up code
    Signed-off-by: Soapthekaiser <jhonperseusmactavish@gmail.com>

--- Natives/AccountListViewController.m
+++ Natives/AccountListViewController.m
@@ -70,7 +70,6 @@
     }
     self.modalInPresentation = YES;
     self.tableView.userInteractionEnabled = NO;
-    [self addActivityIndicatorTo:cell];
     id callback = ^(id status, BOOL success) {
         dispatch_async(dispatch_get_main_queue(), ^(){
             [self callbackMicrosoftAuth:status success:success forCell:cell];
@@ -79,60 +78,13 @@
     [[BaseAuthenticator loadSavedName:self.accountList[indexPath.row][@"username"]] refreshTokenWithCallback:callback];
 }
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        NSString *str = self.accountList[indexPath.row][@"username"];
-        NSFileManager *fm = [NSFileManager defaultManager];
-        NSString *path = [NSString stringWithFormat:@"%s/accounts/%@.json", getenv("POJAV_HOME"), str];
-        if (self.whenDelete != nil) { self.whenDelete(str); }
-        NSString *xuid = self.accountList[indexPath.row][@"xuid"];
-        if (xuid) { [MicrosoftAuthenticator clearTokenDataOfProfile:xuid]; }
-        [fm removeItemAtPath:path error:nil];
-        [self.accountList removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }
-}
-
-- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return (indexPath.row == self.accountList.count) ? UITableViewCellEditingStyleNone : UITableViewCellEditingStyleDelete;
-}
-
-- (NSDictionary *)parseQueryItems:(NSString *)url {
-    NSMutableDictionary *result = [NSMutableDictionary new];
-    NSArray<NSURLQueryItem *> *queryItems = [NSURLComponents componentsWithString:url].queryItems;
-    for (NSURLQueryItem *item in queryItems) { result[item.name] = item.value; }
-    return result;
-}
-
 - (void)actionAddAccount:(UITableViewCell *)sender {
     UIAlertController *picker = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
     [picker addAction:[UIAlertAction actionWithTitle:localize(@"login.option.microsoft", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) { [self actionLoginMicrosoft:sender]; }]];
     [picker addAction:[UIAlertAction actionWithTitle:localize(@"login.option.local", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) { [self actionLoginLocal:sender]; }]];
     [picker addAction:[UIAlertAction actionWithTitle:localize(@"Cancel", nil) style:UIAlertActionStyleCancel handler:nil]];
     picker.popoverPresentationController.sourceView = sender;
-    picker.popoverPresentationController.sourceRect = sender.bounds;
     [self presentViewController:picker animated:YES completion:nil];
-}
-
-- (void)actionLoginLocal:(UIView *)sender {
-    if (getPrefBool(@"warnings.local_warn")) {
-        setPrefBool(@"warnings.local_warn", NO);
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:localize(@"login.warn.title.localmode", nil) message:localize(@"login.warn.message.localmode", nil) preferredStyle:UIAlertControllerStyleActionSheet];
-        alert.popoverPresentationController.sourceView = sender;
-        [alert addAction:[UIAlertAction actionWithTitle:localize(@"OK", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {[self actionLoginLocal:sender];}]];
-        [self presentViewController:alert animated:YES completion:nil];
-        return;
-    }
-    UIAlertController *controller = [UIAlertController alertControllerWithTitle:localize(@"Sign in", nil) message:localize(@"login.option.local", nil) preferredStyle:UIAlertControllerStyleAlert];
-    [controller addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-        textField.placeholder = localize(@"login.alert.field.username", nil);
-    }];
-    [controller addAction:[UIAlertAction actionWithTitle:localize(@"OK", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-        id callback = ^(id status, BOOL success) { self.whenItemSelected(); [self dismissViewControllerAnimated:YES completion:nil]; };
-        [[[LocalAuthenticator alloc] initWithInput:controller.textFields[0].text] loginWithCallback:callback];
-    }]];
-    [controller addAction:[UIAlertAction actionWithTitle:localize(@"Cancel", nil) style:UIAlertActionStyleCancel handler:nil]];
-    [self presentViewController:controller animated:YES completion:nil];
 }
 
 - (void)actionLoginMicrosoft:(UITableViewCell *)sender {
@@ -151,6 +103,13 @@
     [self.authVC start];
 }
 
+- (NSDictionary *)parseQueryItems:(NSString *)url {
+    NSMutableDictionary *result = [NSMutableDictionary new];
+    NSArray<NSURLQueryItem *> *queryItems = [NSURLComponents componentsWithString:url].queryItems;
+    for (NSURLQueryItem *item in queryItems) { result[item.name] = item.value; }
+    return result;
+}
+
 - (void)callbackMicrosoftAuth:(id)status success:(BOOL)success forCell:(UITableViewCell *)cell {
     if (success) {
         self.whenItemSelected();
@@ -159,4 +118,10 @@
         showDialog(localize(@"Error", nil), [status localizedDescription]);
     }
 }
+
+#pragma mark - ASWebAuthenticationPresentationContextProviding
+- (ASPresentationAnchor)presentationAnchorForWebAuthenticationSession:(ASWebAuthenticationSession *)session {
+    return UIApplication.sharedApplication.windows.firstObject;
+}
+
 @end
